6d906f56a79e61ef0d79286d3be47f47620cf8f0
lexsus187 <Alekseyka.alex333@gmail.com>
2025-03-27 19:58:03 +0300

    Add files via upload

--- skript.js
+++ skript.js
@@ -1,14 +1,58 @@
 let tg = window.Telegram.WebApp;
+let btn = document,querySelector("#click");
 
-tg.expand()
+tg.MainButton.textColor = "#FFFFFF"
+tg.MainButton.color = "#2cab37"
 
-let btn = document.querlySelector("#click")
+let item = ""
 
-btn.onclick = () => {
+let btn1 = document.querySelector("#btn1")
+let btn2 = document.querySelector("#btn2")
+let btn3 = document.querySelector("#btn3")
+let btn4 = document.querySelector("#btn4")
+
+btn1.onclick = () => {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide()
     } else {
-        tg.MainButton.setText("your have cate")
+        tg.MainButton.setText("вы выбрали Нижний новгород!")
+        item = "Москва-Нижний новгород"
         tg.MainButton.show()
     }
 }
+
+btn2.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("вы выбрали Казань!")
+        item = "Самара-казань"
+        tg.MainButton.show()
+    }
+}
+
+btn3.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("вы выбрали брянск!")
+        item = "Ижевск-Брянск"
+        tg.MainButton.show()
+    }
+}
+
+btn4.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("вы выбрали Сочи!")
+        item = "Владивосток-Сочи"
+        tg.MainButton.show()
+    }
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    tg.sendData(item);
+})
+
+tg.expand();
